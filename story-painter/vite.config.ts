7cea3901b5980be85933d187aedd56fa7d158cfa
JustAnotherID <just-another-id@outlook.com>
2025-10-09 16:17:06 +0800

    build(ui&manual&story-painter&verify): upgrade dependencies

--- story-painter/vite.config.ts
+++ story-painter/vite.config.ts
@@ -1,50 +1,67 @@
-import path from 'path'
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import legacy from '@vitejs/plugin-legacy'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
-const pathSrc = path.resolve(__dirname, 'src')
-
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   resolve: {
     alias: {
-      '~/': `${pathSrc}/`,
+      '~': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: './',
   plugins: [
     vue(),
     Components({
-      // allow auto load markdown components under `./src/components/`
-      extensions: ['vue', 'md'],
-      // allow auto import and register components used in markdown
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      dirs: ['src/components'], // allow auto load Markdown components under `./src/components/`
+      extensions: ['vue', 'md', 'jsx', 'tsx'], // allow auto import and register components used in Markdown
+      include: [
+        /\.vue$/,
+        /\.vue\?vue/,
+        /\.vue\.[tj]sx?\?vue/, // for vue-loader with experimentalInlineMatchResource enabled
+        /\.vue\?v=/,
+        /\.[tj]sx/,
+      ],
       resolvers: [
         NaiveUiResolver(),
       ],
       dts: 'src/components.d.ts',
     }),
-    // legacy({
-    //   targets: ['defaults', 'not IE 11']
-    // })
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    }),
+    nodePolyfills(),
   ],
   build: {
+    chunkSizeWarningLimit: 1024,
+    rolldownOptions: {
+      output: {
+        advancedChunks: {
+          groups: [
+            { name: 'editer', test: /(codemirror|highlight)/ },
+            { name: 'import-export', test: /(docx|fflate|hyparquet)/ },
+            { name: 'naive-ui', test: /naive-ui/ },
+          ],
+        },
+      },
+    },
     sourcemap: false,
   },
   server: {
     proxy: {
 
       '/api': {
-          changeOrigin: true,
-          target: 'https://worker.firehomework.top/dice/api',
-          // target: 'http://8.130.140.128:8082',
-          // target: 'http://localhost:8088',
+        changeOrigin: true,
+        target: 'https://worker.firehomework.top/dice/api',
+        // target: 'http://8.130.140.128:8082',
+        // target: 'http://localhost:8088',
 
-          rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ''),
 
       },
     }
